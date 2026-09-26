a8715418af2dc17c9161f395259f789a8c15a1fd
Irnes Sarvan <115439980+irnessarvan13@users.noreply.github.com>
2026-09-25 21:46:11 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        L = 0 
+        R = 0
+        maxCount = 0
+        result = 0
+        myHash = {}
+
+        for R in range(len(s)):
+            if s[R] not in myHash:
+                myHash[s[R]] = 1
+            else:
+                myHash[s[R]] += 1
+            maxCount = max(maxCount, myHash[s[R]])
+            while (R - L + 1) - maxCount > k:
+                myHash[s[L]] -= 1
+                L += 1
+            result = max(result, R - L + 1)
+        return result
